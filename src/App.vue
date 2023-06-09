7e78688066840ac070ac6b0e46d0a326847bbe6e
Nemesis-Roleplay <bolospaul7@yahoo.com>
2023-06-09 21:24:28 +0300

    better spectrum

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@
     <div class="dot-grid"></div>
       <div class="main">
         <div id="spectrum">
-        <video  autoplay muted loop>
+        <video autoplay muted loop>
           <source src="./assets/Orb.mp4" type="video/mp4">
           Your browser does not support the video tag.
         </video>
+        <div class="glow-spehere"></div>
       </div>
     </div>
   </div>
@@ -26,7 +27,7 @@
 
 <style>
 body {
-  background: rgb(2,0,36);
+  background: rgb(1, 0, 27);
   margin: 0;
 }
 .dot-grid {
@@ -46,5 +47,24 @@ body {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+video {
+  border-radius: 360px;
+  height: 20rem;
+  border: 1px solid rgba(128, 128, 128, 0.1);
+}
+
+.glow-spehere {
+  height: 20rem;
+  width: 20rem;
+  background: rgb(120,173,255);
+  background: radial-gradient(circle, rgba(120,173,255,1) 0%, rgb(103, 100, 255) 50%, rrgb(43, 0, 255)100%);
+  position: absolute;
+  top: calc(50% - 10rem - 0.5px);
+  left: calc(50% - 10rem - 0.5px);
+  border-radius: 50%;
+  opacity: 0.1;
+  box-shadow: 0px 0px 80px 30px rgb(94, 163, 236);
 }
 </style>
